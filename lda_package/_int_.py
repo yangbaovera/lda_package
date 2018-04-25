a14d2b076aa8229b8949b174d4a9834ba7686799
yangbaovera <31319977+yangbaovera@users.noreply.github.com>
2018-04-25 03:52:46 -0400

    add package

---
+++ lda_package/_int_.py
@@ -0,0 +1,5 @@
+
+from .LDA import is_convergence1, is_convergence2, optimize_vp
+from .LDA import alpha_estimate, alpha_estimate_opt
+from .LDA import beta_estimate, beta_estimate_opt
+from .LDA import log_sum, optimize_vp_opt
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = "mylda_package",
+      version = "1.0",
+      author='Yang Bao, Wenlin Wu',
+      url='#####',
+      description='Implementation of Latent Dirichlet Allocation',
+      packages=setuptools.find_packages()
+      )
