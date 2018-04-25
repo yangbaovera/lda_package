047da3a1eb69f8357109c7b5ab7c1c6feb4ebe1d
yangbaovera <31319977+yangbaovera@users.noreply.github.com>
2018-04-25 10:18:10 -0400

    Update __init__.py

--- lda_package/__init__.py
+++ lda_package/__init__.py
@@ -1,5 +1,7 @@
-from .LDA import is_convergence1, is_convergence2, optimize_vp
-from .LDA import alpha_estimate, alpha_estimate_opt
-from .LDA import beta_estimate, beta_estimate_opt
-from .LDA import log_sum, optimize_vp_opt
-from .LDA import variation_EM, variation_EM_new
+import LDA
+from LDA import is_convergence1
+from LDA import is_convergence2, optimize_vp
+from LDA import alpha_estimate, alpha_estimate_opt
+from LDA import beta_estimate, beta_estimate_opt
+from LDA import log_sum, optimize_vp_opt
+from LDA import variation_EM, variation_EM_new
