8c1f8c8f8842b59de382ae7b73b5606d12122393
yangbaovera <31319977+yangbaovera@users.noreply.github.com>
2018-04-25 10:14:59 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
+
 from setuptools import setup, find_packages
 
-setup(name = "lda_package",
+setup(
+      name = "lda_package",
       version = "1.0",
       author='Yang Bao, Wenlin Wu',
       url='https://github.com/yangbaovera/lda_package',
       description='Implementation of Latent Dirichlet Allocation',
-      packages=setuptools.find_packages()
+      packages=find_packages(),
+      py_modules = ['lda_package'],
+      install_requires=[]
       )
